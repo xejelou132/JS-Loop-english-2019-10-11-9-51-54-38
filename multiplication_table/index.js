efd5a8a49eab166fd320385d535d33aac73807fd
Darlen Kitz Suizo <suizoda@oocl.com>
2021-07-22 18:11:53 +0800

    Loop exercises

---
+++ multiplication_table/index.js
@@ -0,0 +1,8 @@
+for(var i = 1; i <10; i++){
+   for (var x = 1; x <10; x++){
+    if(x<=i){
+    document.write(i +" * " +x +" = " + x*i + " ");
+  }
+   }
+   document.write("<br>");
+}
---
+++ number/index.js
@@ -0,0 +1,8 @@
+for(var i = 1; i <=20; i++){
+ if (i %2 == 0){
+  document.write(i + " is even number <br>");
+}
+else{
+  document.write(i + " is odd number <br>");
+}
+}
